179c15c0276ae0639b6d2ee7f47a890a5ce1a00f
Kai Havertz <zudeeeeee@gmail.com>
2026-08-28 10:28:07 +0700

    weekly monitoring

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,17 +1,54 @@
-const activityModel = require("../models/monitoringModel");
+const models = require("../models/monitoringModel");
+
+const getWeekRange = () => {
+    const today = new Date();
+
+    const day = today.getDay();
+
+    const diffToMonday =
+        day === 0
+            ? -6
+            : 1 - day;
+
+    const monday = new Date(today);
+
+    monday.setDate(
+        today.getDate() + diffToMonday
+    );
+
+    monday.setHours(0, 0, 0, 0);
+
+    const sunday = new Date(monday);
+
+    sunday.setDate(
+        monday.getDate() + 6
+    );
+
+    const formatDate = (date) => {
+        return date
+            .toISOString()
+            .split("T")[0];
+    };
+
+    return {
+        startDate: formatDate(monday),
+        endDate: formatDate(sunday)
+    };
+};
 
 const index = async (req, res) => {
     try {
+        const user_id = req.session.user.id;
         const today = new Date()
             .toISOString()
             .split("T")[0];
-
-        const monitoring = await activityModel.getDailyMonitoring(
-            req.session.user.id,
+        //daily
+        const dailyRows = await models.getDailyMonitoring(
+            user_id,
             today
         );
 
-        const dailyMonitoring = monitoring.map(item => {
+        const dailyMonitoring = dailyRows.map(item => {
 
             const actualValue = Number(item.actual_value);
             const targetValue = Number(item.target_value);
@@ -38,10 +75,63 @@ const index = async (req, res) => {
             };
         });
 
-        res.render("dashboard/index", {
-            user: req.session.user,
-            monitoring: dailyMonitoring
-        });
+        //weekly    
+        const { startDate, endDate } = getWeekRange();
+
+        const weeklyRows =
+            await models.getWeeklyMonitoring(
+                user_id,
+                startDate,
+                endDate
+            );
+        
+        const weeklyMonitoring =
+            weeklyRows.map(item => {
+
+                const actualValue =
+                    Number(item.actual_value);
+
+                const targetValue =
+                    Number(item.target_value);
+
+                let status;
+
+                if (actualValue === 0) {
+                    status = "red";
+
+                } else if (
+                    actualValue < targetValue
+                ) {
+                    status = "yellow";
+
+                } else {
+                    status = "green";
+                }
+
+                const progressPercentage =
+                    (actualValue / targetValue) * 100;
+
+                return {
+                    ...item,
+                    actualValue,
+                    targetValue,
+                    status,
+                    progressPercentage
+                };
+            });
+
+            console.log("Weekly Monitoring:", weeklyMonitoring);
+
+         res.render(
+            "dashboard/index",
+            {
+                user: req.session.user,
+                dailyMonitoring,
+                weeklyMonitoring,
+                startDate,
+                endDate
+            }
+        );
 
     } catch (error) {
         console.error(error);
--- src/models/monitoringModel.js
+++ src/models/monitoringModel.js
@@ -36,6 +36,53 @@ const getDailyMonitoring = async (userId, date) => {
     return rows;
 };
 
+const getWeeklyMonitoring = async (
+    userId,
+    startDate,
+    endDate
+) => {
+    const [rows] = await promisePool.query(
+        `SELECT
+            categories.id AS category_id,
+            categories.name,
+            categories.icon,
+
+            targets.target_value,
+
+            COUNT(
+                DISTINCT activities.activity_date
+            ) AS actual_value
+
+        FROM categories
+
+        JOIN targets
+            ON targets.category_id = categories.id
+            AND targets.user_id = categories.user_id
+
+        LEFT JOIN activities
+            ON activities.category_id = categories.id
+            AND activities.user_id = categories.user_id
+            AND activities.activity_date BETWEEN ? AND ?
+
+        WHERE categories.user_id = ?
+        AND targets.period = 'weekly'
+
+        GROUP BY
+            categories.id,
+            categories.name,
+            categories.icon,
+            targets.target_value`,
+        [
+            startDate,
+            endDate,
+            userId
+        ]
+    );
+
+    return rows;
+};
+
 module.exports = {
-    getDailyMonitoring
+    getDailyMonitoring,
+    getWeeklyMonitoring
 };
